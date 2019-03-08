8e02f73cb0df8b3cc20df07d6f27e6116433acbd
Jean Santos <jeancarlosaps@yahoo.com.br>
2019-03-08 16:11:43 -0300

    Created characters search screen;

---
+++ MarvelHeroes/MarvelHeroes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MarvelHeroes
+//
+//  Created by Jean Carlos Antonio Pereira dos Santos on 08/03/19.
+//  Copyright © 2019 Jean Carlos Antonio Pereira dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  //MARK: - Properties
+
+  //MARK: - IBOutlets
+  @IBOutlet weak var tfSearchCharacters: UITextField!
+  
+  //MARK: - Super Methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
